d07ea6ee57cc9a4ccecd4b68d1faf1f48434c4c5
Eleonilson <eleonilson.r.l@gmail.com.br>
2024-08-25 23:31:32 -0300

    Controle de fluxo

---
+++ controle-fluxo/src/ExemploBreakContinue.java
@@ -0,0 +1,12 @@
+public class ExemploBreakContinue {
+  // class ExemploBreakContinue.java
+  public static void main(String[] args) {
+    for (int numero = 1; numero <= 5; numero++) {
+      if (numero == 3)
+        //break;
+        continue;
+
+      System.out.println(numero);
+    }
+  }
+}
