25f2265a263a7e9a822dbcc8cc8e58e348fa5336
Eleonilson <eleonilson.r.l@gmail.com.br>
2024-09-05 23:07:59 -0300

    Desafio versão 2

--- calculo-salarial-java/desafio/src/Desafio.java
+++ calculo-salarial-java/desafio/src/Desafio.java
@@ -15,14 +15,10 @@ public class Desafio {
         float valorImposto = 0;
         if (valorSalario >= 0 && valorSalario <= 1100) {
             // Atribuir a aliquota de 5% mediante ao salário:
-            valorImposto = 0.5f * valorSalario;
-        }
-        if (valorSalario >= 1100.01 && valorSalario <= 2500) {
-            // Atribuir a aliquota de 10% mediante ao salário:
+            valorImposto = 0.05f * valorSalario;
+        } else if (valorSalario >= 1100.01 && valorSalario <= 2500) {
             valorImposto = 0.10f * valorSalario;
-        }
-        if (valorSalario > 2501) {
-            // Atribuir a aliquota de 15% mediante ao salário:
+        } else {
             valorImposto = 0.15f * valorSalario;
         }
         // TODO Criar as demais condições para as aliquotas de 10.00% e 15.00%.
