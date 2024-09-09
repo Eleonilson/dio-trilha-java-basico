9ecbe5ff7fa2e885b84c72e89955b14739d6b5f6
Eleonilson <eleonilson.r.l@gmail.com.br>
2024-09-09 00:50:39 -0300

    versão final

--- VerificacaoServico/src/VerificacaoServico.java
+++ VerificacaoServico/src/VerificacaoServico.java
@@ -2,22 +2,35 @@ import java.util.Scanner;
 
 public class VerificacaoServico {
     public static void main(String[] args) throws Exception {
-         Scanner scanner = new Scanner(System.in);
-        
+        Scanner scanner = new Scanner(System.in);
+
         // Entrada do serviço a ser verificado
         String servico = scanner.nextLine().trim();
-        
+
         // Entrada do nome do cliente e os serviços contratados
         String entradaCliente = scanner.nextLine().trim();
-        
+
         // Separando o nome do cliente e os serviços contratados
         String[] partes = entradaCliente.split(",");
         String nomeCliente = partes[0];
         boolean contratado = false;
-        
+
         // TODO: Verifique se o serviço está na lista de serviços contratados
-        
+        for (int i = 1; i < partes.length; i++) {
+            if (partes[i].trim().equalsIgnoreCase(servico)) {
+                contratado = true;
+                break;
+            }
+        }
+
+        // Saída: "Sim" se o seviço foi encontrado, "Não" caso contrario.
+        if (contratado) {
+            System.out.println("Sim");
+        } else {
+            System.out.println("Não");
+        }
+
         scanner.close();
-    
+
     }
 }
